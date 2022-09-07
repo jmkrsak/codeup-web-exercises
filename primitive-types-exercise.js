43d53a8f49d98e8e33afeb5f9d0ec516e1d1c791
John-Michael Krsak <jmkrsak@gmail.com>
2022-09-07 16:37:07 -0400

    primitive type questions

---
+++ primitive-types-exercise.js
@@ -0,0 +1,27 @@
+typeof 99.9
+'number'
+typeof "false"
+'string'
+typeof false
+'boolean'
+typeof '0'
+'string'
+typeof 0
+'number'
+typeof true
+'boolean'
+typeof 'true'
+'string'
+
+//A term or phrase typed into a search box?
+    //string
+//If a user is logged in?
+    //boolean
+//A discount amount to apply to a user's shopping cart?
+    //number
+//Whether or not a coupon code is valid?
+    //boolean
+//An email address typed into a registration form?
+    //string
+//The price of a product?
+    //number
