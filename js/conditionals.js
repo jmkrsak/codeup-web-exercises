00e76c04731b7798945b385b8709b1c534ef2a1f
John-Michael Krsak <jmkrsak@gmail.com>
2022-09-13 09:52:55 -0400

    condtionals exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -20,6 +20,10 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
+function analyzeColor(color){
+
+}
+
 
 // Don't change the next two lines!
 // These lines create two variables for you:
