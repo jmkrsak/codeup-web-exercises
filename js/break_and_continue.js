ced3cd39a47e66a93a5819e51b0d6c2e669c354a
John-Michael Krsak <jmkrsak@gmail.com>
2022-09-19 08:45:03 -0400

    added break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,88 @@
 "use strict"
+// let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+// console.log("The number chosen was: "+oddNum);
+// for (let i = 1; i < 50; i+=2) {
+//     while((oddNum % 2 === 0 || oddNum > 50 || oddNum < 1)) {
+//         alert("This is not a vaild number, you didn't follow the rules!");
+//         oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+//         console.log("The number chosen was: "+oddNum);
+//         continue;
+//     }while(i === oddNum){
+//         console.log("Yikes! Skipping number: "+ oddNum);         break;
+//     }if(i !== oddNum){
+//         console.log("here is and odd number: "+ i);
+//     } }
+
+let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+console.log("The number chosen was: "+oddNum);
+for (let i = 1; i < 50; i+=2) {
+    if ((oddNum % 2 === 0 || oddNum > 50 || oddNum < 1)) {
+        alert("This is not a vaild number, you didn't follow the rules!");
+        oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+        console.log("The number chosen was: "+oddNum);
+        continue;
+    } else if (i === oddNum) {
+        console.log("Yikes! Skipping number: "+ oddNum);
+        // console.log("here is and odd number: "+ i);
+        break;
+    } else if (i !== oddNum) {
+        console.log("here is and odd number: "+ i);
+    }
+}
+
+
+// let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+//
+// for (oddNum = 1; oddNum < 50; oddNum) {
+//     if (oddNum % 2 === 0) {
+//         alert ("This is not a vaild number, you didn't follow the rules!");
+//         let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+//     } else if (oddNum >= 50) {
+//         alert ("This is not a vaild number, you didn't follow the rules!");
+//         let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+//     } else if (oddNum < 1) {
+//         alert ("This is not a vaild number, you didn't follow the rules!");
+//         let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+//     } else {
+//         alert ("Alright, you picked a valid number!")
+//     }
+// }
+// let oddNum = parseInt(prompt("Input an Odd Number between 1 and 50."));
+// if (oddNum % 2 === 0) {
+//     alert ("This is not a vaild number, you didn't follow the rules!");
+//     alert (oddNum);
+// } else if (oddNum >= 50) {
+//     alert ("This is not a vaild number, you didn't follow the rules!");
+//     alert (oddNum);
+// } else if (oddNum < 1) {
+//     alert ("This is not a vaild number, you didn't follow the rules!");
+//     alert (oddNum);
+// } else {
+//     alert ("Alright, you picked a valid number!")
+// }
+// for(let i = 0; i < 10; i++){
+//     alert("i is currently: " + i);
+//     if(i === 5){
+//         alert("Stopping at 5");
+//         break;
+//     }
+// }
+// for(let i = 0; i < 10; i++){
+//     alert("i is currently: " + i);
+//     if(i === 5){
+//         alert("5 is cool");
+//         continue;
+//     }
+// }
 // function isEven(num) {
 //     return (parseInt(num) % 2 === 0);
-//
-//
-// prompt ("Give me an odd number, any ODD NUMBER!");
+// }
+
+// // Get a random integer from 1 to 10 inclusive
+// const num = Math.ceil(Math.random() * 10);
+// console.log(num);
+// const gnum = prompt('Guess the number between 1 and 10 inclusive');
+// if (gnum == num)
+//     console.log('Matched');
+// else
+//     console.log('Not matched, the number was '+gnum);
