43d2eac25d903e61b40d29110d6a9f20abe01af3
John-Michael Krsak <jmkrsak@gmail.com>
2022-09-22 17:41:18 -0400

    study guide

---
+++ js/studyguide1.js
@@ -0,0 +1,61 @@
+"use strict"
+
+// Core competencies assessed
+// Reading and following directions
+// Producing solutions that match the problem specification
+// Using Git and working with GitHub
+//     Cloning a git repository from GitHub
+// Creating your own branch
+// Committing and pushing work to a new branch on GitHub
+// Programing fundamentals in JavaScript
+// Working with values, variables, and data types
+// Using assignment and comparision operators
+// Working with JS internal functions to make decisions and perform actions.
+//     Using conditional logic to make decisions inside functions
+// Writing functions that take in inputs, process, and return outputs.
+//     Here's some example word problems:
+
+//----------//
+
+// Define a function named timesFour that takes in an input and multiples that input by four if it is numeric. If the input is not numeric, then return false.
+// timesFour(0) //0
+// timesFour("5") //20
+// timesFour(-5) //-20
+// timesFour('Texas') //false
+// timesFour([2,1,0]) //false
+// timesFour(true) //false
+// timesFour(null) //false
+
+function timesFour (x) {
+    if (typeof parseInt(x) == "number") {
+        return x * 4
+    } else if (typeof parseInt(x) != "number") {
+        return false
+    }
+}
+
+timesFour();
+
+//----------//
+
+// Define a function named convertDaystoHours that takes in one input Days. Return the conversion of the number of days into total hours. If the input is not numeric or a numeric string, convertDaystoHours, should return false.
+// convertDaystoHours(0) //0
+// convertDaystoHours('5') // 120
+// convertDaystoHours('Texas') //false
+// convertDaystoHours([2, 1, 0]) //false
+// convertDaystoHours(true) //false
+// convertDaystoHours(false) //false
+
+//----------//
+
+// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
+// calculateTax(25, 8)               // “$27.00"
+// calculateTax(10, 12)             // “$11.20”
+// calculateTax(120, 15.5)           // “$138.60"
+// calculateTax(10, true)           // false
+// calculateTax([1, 2, 3], 10)      // false
+// calculateTax(“Codeup”, 100)      // false
+// calculateTax()                   // false
+
+let schoolName = `codeup`;
+console.log(`Hi my name is John and I attend ${schoolName}`);
