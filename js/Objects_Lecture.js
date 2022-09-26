aca81274d2ed65b82c0dbdcb69053c34dba01969
John-Michael Krsak <jmkrsak@gmail.com>
2022-09-26 10:26:39 -0400

    daily warmups

--- js/Objects_Lecture.js
+++ js/Objects_Lecture.js
@@ -46,34 +46,34 @@
 //     programLength: 6,
 // }
 
-let airplane = {
-    model: 747,
-    brand: "boeing",
-    seats: 120,
-    color: "blue and white",
-    crew: ["pilots", "flight attendants"],
-    passengers: [
-        {
-            name: "Ethan",
-            age: 21,
-            location: "brownsville",
-            sayAge: function(){
-                return "hello, I am " + this.age + " years old";
-            }
-        },
-        {
-            name: "Jordan",
-            age: 28,
-            location: "Pana, Il",
-        },
-        {
-            name: "Riley",
-            age: 18,
-            location: "Hartly, De",
-        },
-    ]
-
-}
+// let airplane = {
+//     model: 747,
+//     brand: "boeing",
+//     seats: 120,
+//     color: "blue and white",
+//     crew: ["pilots", "flight attendants"],
+//     passengers: [
+//         {
+//             name: "Ethan",
+//             age: 21,
+//             location: "brownsville",
+//             sayAge: function(){
+//                 return "hello, I am " + this.age + " years old";
+//             }
+//         },
+//         {
+//             name: "Jordan",
+//             age: 28,
+//             location: "Pana, Il",
+//         },
+//         {
+//             name: "Riley",
+//             age: 18,
+//             location: "Hartly, De",
+//         },
+//     ]
+//
+// }
 //
 // airplane.crew.forEach(function(member){
 //     console.log("Thats so cool that youre a: " + member);
@@ -113,5 +113,29 @@ let airplane = {
 
 //----MATH----
 
+// morning exercise
 
+// Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+// example input: [1,2,3,4,5] expected output: [2,4]
+
+function returnEven (array) {
+    let myArray = [1,2,3,4,5,6,7,8,9,10];
+    let even = [];
+    for (let i = 0; i < myArray.length; i++) {
+        const num = myArray[i];
+        if (num % 2 === 0) {
+            even.push(num);
+        }
+    }
+    console.log(even);
+}
+returnEven();
+
+// Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+//     example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+
+function addContinent(){
+
+}
 
