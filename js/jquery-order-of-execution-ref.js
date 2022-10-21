c7842416c34392adeb55e3f82ead3a171731eec2
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-21 11:22:51 -0400

    new exercise

---
+++ js/jquery-order-of-execution-ref.js
@@ -0,0 +1,27 @@
+// step 7.
+// window.onload = function(){
+//     alert('windown.onload');
+// }
+
+// function anotherFunction(){
+//     alert('another function just ran');
+// }
+
+// step 1 - run anotherfunction
+// anotherFunction();
+
+// step 2
+// alert('global alert')
+
+// step 4
+// $(function() {
+    // step 5
+    // alert( 'The DOM has finished loading! -> jquery' );
+    // step 6
+    // console.log("inside jquery fn")
+    // console.log($('#download-jquery-btn'));
+// });
+
+// step 3
+// console.log('global:')
+// console.log($('#download-jquery-btn'));
