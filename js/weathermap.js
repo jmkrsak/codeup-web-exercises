b6f09980de0bec1255e3b6e1d86ea001c0a3df85
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-26 17:18:29 -0400

    weather map project

--- js/weathermap.js
+++ js/weathermap.js
@@ -5,42 +5,69 @@
 // const key = process.env.API_KEY;
 //
 // console.log("sanity check");
+// -----weather objects made into html-----
+
+
 
 // ----- map box -----
 
 mapboxgl.accessToken = MAP_KEY;
 
-geocode("2111 Kirkwood St, High Point, NC 27262", MAP_KEY).then(function(result){
-    console.log(result)
-    let map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
-        zoom: 15,
-        center: result,
+let zoomSet = 15;
+let defaultCenter = [-80.02171, 35.9838];
+// let locationInput = $('#search-box').val();
+
+function mapOnPageLoad() {
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: zoomSet,
+    center: defaultCenter,
     });
-    const marker = new mapboxgl.Marker()
-        .setLngLat(result)
-        .setPopup(new mapboxgl.Popup().setHTML("<h1>Korean BBQ</h1>"))
-        .addTo(map);
-});
+}
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+// let coords = geocode(locationInput, MAP_KEY).then(function(result){
+//     let locationInput = $('#search-box').val();
+//     return result
+// })
+$('#searchBtn').click(function(e){
+    e.preventDefault();
+    let locationInput = $('#search-box').val();
+    geocode(locationInput, MAP_KEY).then(function(result){
+        let map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            zoom: zoomSet,
+            center: result,
+        });
+    });
+})
 
-// ----- weather map -----
-
-console.log(API_KEY);
+//----- weather map -----
 
 let myTown = "New Orleans";
 console.log(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`);
 
-$.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${myTown}&appid=${API_KEY}`).done(function(data){
+let fiveDayData = $.ajax(`https://api.openweathermap.org/data/2.5/forecast?q=${myTown}&appid=${API_KEY}`).done(function(data){
     console.log(data);
+    return data.list
 })
+
+console.log(fiveDayData);
 
 $("#searchBtn").click(function(e){
     e.preventDefault();
     console.log($("#searchBtn").val());
-
     let inputStr = $("#searchBox").val();
-
-
 })
 
+mapOnPageLoad();
