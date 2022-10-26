a4d4f20c91c780bb9e41858bb63b8b157524e6c2
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-26 11:51:51 -0400

    updating weather map project

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
-console.log(process.env);
-const key = process.env.API_KEY;
-
-console.log("sanity check");
+// require('dotenv').config()
+//
+// require('dotenv').config()
+// console.log(process.env);
+// const key = process.env.API_KEY;
+//
+// console.log("sanity check");
 
