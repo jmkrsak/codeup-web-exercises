0b79c501f7dee8e70ac21b717baa25abde4fad72
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-26 12:33:41 -0400

    fix repo

--- js/weathermap.js
+++ js/weathermap.js
@@ -6,3 +6,41 @@
 //
 // console.log("sanity check");
 
+// ----- map box -----
+
+mapboxgl.accessToken = MAP_KEY;
+
+geocode("2111 Kirkwood St, High Point, NC 27262", MAP_KEY).then(function(result){
+    console.log(result)
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 15,
+        center: result,
+    });
+    const marker = new mapboxgl.Marker()
+        .setLngLat(result)
+        .setPopup(new mapboxgl.Popup().setHTML("<h1>Korean BBQ</h1>"))
+        .addTo(map);
+});
+
+// ----- weather map -----
+
+console.log(API_KEY);
+
+let myTown = "New Orleans";
+console.log(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`);
+
+$.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${myTown}&appid=${API_KEY}`).done(function(data){
+    console.log(data);
+})
+
+$("#searchBtn").click(function(e){
+    e.preventDefault();
+    console.log($("#searchBtn").val());
+
+    let inputStr = $("#searchBox").val();
+
+
+})
+
