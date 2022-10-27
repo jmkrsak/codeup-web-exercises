fc1d25f53ef01efae4edf81def8851a74a5cff6f
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-27 12:03:09 -0400

    update weather project

--- js/weathermap.js
+++ js/weathermap.js
@@ -15,16 +15,22 @@ mapboxgl.accessToken = MAP_KEY;
 
 let zoomSet = 15;
 let defaultCenter = [-80.02171, 35.9838];
+
+let html= '';
+html += '';
+html += '';
+
 // let locationInput = $('#search-box').val();
 
 function mapOnPageLoad() {
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: zoomSet,
-    center: defaultCenter,
-    });
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: zoomSet,
+        center: defaultCenter,
+        });
 }
+
 function geocode(search, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
@@ -55,19 +61,31 @@ $('#searchBtn').click(function(e){
 //----- weather map -----
 
 let myTown = "New Orleans";
-console.log(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`);
+console.log(`https://api.openweathermap.org/data/2.5/weather?q=${myTown}&appid=${API_KEY}`);
 
-let fiveDayData = $.ajax(`https://api.openweathermap.org/data/2.5/forecast?q=${myTown}&appid=${API_KEY}`).done(function(data){
+$.ajax(`https://api.openweathermap.org/data/2.5/forecast?q=${myTown}&appid=${API_KEY}`).done(function(data){
     console.log(data);
-    return data.list
-})
+    console.log(data.list[0].dt_txt.split(" ")[0]);
+    console.log(`${data.list[0].main.temp_min} ${data.list[0].main.temp_max}`);
+    console.log(data.list[0].weather[0].description);
+    console.log(data.list[0].main.humidity);
+    console.log(data.list[0].wind.speed);
+    console.log(data.list[0].main.pressure);
 
-console.log(fiveDayData);
-
-$("#searchBtn").click(function(e){
-    e.preventDefault();
-    console.log($("#searchBtn").val());
-    let inputStr = $("#searchBox").val();
+    // let weatherData = data.list[0].dt_txt.split(" ")[0];
+    // let minMaxTemp = `${data.list[0].main.temp_min} ${data.list[0].main.temp_max}`;
+    // let weatherDescription = data.list[0].weather[0].description;
+    // let weatherHumidity = data.list[0].main.humidity;
+    // let weatherSpeed = data.list[0].wind.speed;
+    // let weatherPressure = data.list[0].main.pressure;
 })
+//
+// console.log(fiveDayData);
+//
+// $("#searchBtn").click(function(e){
+//     e.preventDefault();
+//     console.log($("#searchBtn").val());
+//     let inputStr = $("#searchBox").val();
+// })
 
 mapOnPageLoad();
