123b09bdec88409a9c1c983139e659e2030cca36
John-Michael Krsak <jmkrsak@gmail.com>
2022-10-30 16:59:02 -0400

    adding functionality to weather map project

--- js/weathermap.js
+++ js/weathermap.js
@@ -15,12 +15,17 @@ mapboxgl.accessToken = MAP_KEY;
 
 let zoomSet = 15;
 let defaultCenter = [-80.02171, 35.9838];
+let date;
+let tempMin;
+let tempMax;
+let icon;
+let description;
+let humidity;
+let wind;
+let pressure;
+let newArr = [];
+let forcastDays = 5;
 
-let html= '';
-html += '';
-html += '';
-
-// let locationInput = $('#search-box').val();
 
 function mapOnPageLoad() {
     let map = new mapboxgl.Map({
@@ -60,25 +65,53 @@ $('#searchBtn').click(function(e){
 
 //----- weather map -----
 
-let myTown = "New Orleans";
-console.log(`https://api.openweathermap.org/data/2.5/weather?q=${myTown}&appid=${API_KEY}`);
-
-$.ajax(`https://api.openweathermap.org/data/2.5/forecast?q=${myTown}&appid=${API_KEY}`).done(function(data){
+$.ajax({
+    url: `https://api.openweathermap.org/data/2.5/forecast`,
+    type: "GET",
+    data: {
+        appid: API_KEY,
+        lat: 35.9838,
+        lon: -80.02171,
+        units: "imperial",
+    }
+}).done(function(data) {
     console.log(data);
     console.log(data.list[0].dt_txt.split(" ")[0]);
-    console.log(`${data.list[0].main.temp_min} ${data.list[0].main.temp_max}`);
+    console.log(`${data.list[0].main.temp_min}`);
+    console.log(`${data.list[0].main.temp_max}`);
     console.log(data.list[0].weather[0].description);
     console.log(data.list[0].main.humidity);
     console.log(data.list[0].wind.speed);
     console.log(data.list[0].main.pressure);
+    $("#weather-data").empty();
+    data.list.forEach(function (unit) {
+        if  (unit.dt_txt.split(" ")[1] === '00:00:00' && forcastDays !== 0) {
+            date = unit.dt_txt.split(" ")[0];
+            tempMin= `${unit.main.temp_min}`;
+            tempMax = `${unit.main.temp_max}`;
+            icon = unit.weather[0].icon;
+            description = unit.weather[0].description;
+            humidity = unit.main.humidity;
+            wind = unit.wind.speed;
+            pressure = unit.main.pressure;
 
-    // let weatherData = data.list[0].dt_txt.split(" ")[0];
-    // let minMaxTemp = `${data.list[0].main.temp_min} ${data.list[0].main.temp_max}`;
-    // let weatherDescription = data.list[0].weather[0].description;
-    // let weatherHumidity = data.list[0].main.humidity;
-    // let weatherSpeed = data.list[0].wind.speed;
-    // let weatherPressure = data.list[0].main.pressure;
+            let html = "";
+            html += '<div class="col card-header"><p class="mb-4 card-header">' + "<strong>" + date +"</strong>" + "</p>";
+            html += '<p class="mb-2">' + "<strong>" + tempMin + '˚F' + "</strong>" + ' | ' + "<strong>" + tempMax + '˚F' + "</strong>" + '</p>';
+            html += '<img src="http://openweathermap.org/img/w/' + icon + '.png">'
+            html += '<p class="mb-2">' + 'Desciption: ' + "<strong>" + description + "</strong>" + '</p>';
+            html += '<p class="mb-2">' + 'Humdidity: ' + "<strong>" + humidity + "</strong>" + '</p>';
+            html += '<p class="mb-2"> ' + 'Wind: ' + "<strong>" + wind + "</strong>" + '</p>';
+            html += '<p>' + 'Pressure: ' + "<strong>" + pressure + "</strong>" + '</p>';
+            html += '</div>';
+
+            $('#weather-data').append(html);
+            forcastDays--;
+        }
+    })
 })
+this.marker = new mapboxgl.Marker();
+this.
 //
 // console.log(fiveDayData);
 //
