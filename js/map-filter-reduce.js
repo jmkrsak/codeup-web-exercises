0ecdc4bfbc6396d48229b976d784405f7a65595e
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-01 11:34:44 -0400

    refactored exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -48,6 +48,18 @@ console.log(yearsExp);
 let longestEmail = users.reduce((x, e) => x.email.length > e.email.length ? x : e).email
 console.log(longestEmail);
 
-let userNameStr = users.reduce((x, e) => x.push(e.name) ? x)
-console.log(userNameStr);
+// let userNamesStr = users.reduce(function (accum, ele) {
+//     if (users.indexOf(ele) !== users.length -1) {
+//         accum += ele.name + ", "
+//     } else {
+//         accum += ele.name + ". "
+//     }
+//     return accum
+// }, "Your instructors are: ")
+
+// let userNamesStr = users.reduce((names, user) => {(users.indexOf(user) !== users.length -1) ? names += user.name + ", ": names += user.name + "."; return names;}, "Your instructors are: ")
+
+let userNameStr = users.reduce((accum, ele, index) => index !== users.length -1 ? accum += ele.name + ", " : accum += ele.name + ".", "Your instructors are: ");
+
+console.log(userNameStr)
 
